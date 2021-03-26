98e2f50daf7a47beb4ff073ce4d9008268cdb2cc
zarminta <18102217@ittelkom-pwt.ac.id>
2021-03-26 23:54:39 +0700

    Create todo elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState } from 'react'
+import classnames from 'classnames'
 
 import './App.css'
 import shoppingIcon from './assets/shopping-icon.svg'
+import plusIcon from './assets/plus-icon.svg'
+import minusIcon from './assets/minus-icon.svg'
 
 function App() {
   const [value, setValue] = useState("")
@@ -32,11 +35,22 @@ function App() {
 
         {todos.length > 0 ? (
           <div className="todos">
-            {todos.map(todo => {
+            {todos.map((todo, index, arr) => {
               return (
-                <div>
+                <div key={index} className={`todo ${!(arr.length === index + 1) && 'todo-divider'}`}>
                   {todo.title}
-                  {todo.count}
+
+                  <div className="todo-icon-wrapper">
+                    <div className="todo-count">{todo.count}</div>
+
+                    <button className="todo-action-button">
+                    <img src={minusIcon} alt="minus icon"/>
+                    </button>
+
+                    <button className="todo-action-button">
+                      <img src={plusIcon} alt="minus icon"/>
+                    </button>
+                  </div>
                 </div>
               )
             })}
