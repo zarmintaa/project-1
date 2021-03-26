e343a19141eae3aa154a7642c1208c2489675457
zarminta <18102217@ittelkom-pwt.ac.id>
2021-03-26 22:43:18 +0700

    add button and simple use state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <h1>Hello, world!</h1>
-      <h1>Hello, world!</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Plus</button>
+      <button onClick={() => setCount(count - 1)}>Minus</button>
     </div>
   );
 }
