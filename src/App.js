3c4b93b66d0a4a95ddb34d4bf2bdd363fdefc91c
zarminta <18102217@ittelkom-pwt.ac.id>
2021-03-26 22:54:57 +0700

    add nav elements

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ function App() {
         <img className="nav-icon" src={shoppingIcon} alt="shopping icon"/>
         <h1 className="nav-title">Shopping List</h1>
       </nav>
+
+      <section className="container">
+        
+      </section>
     </>
   );
 }
